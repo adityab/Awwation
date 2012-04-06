4f3a23f2f714ccb11172ca6927b9638bacd9afc5
adityab <aditya@adityabhatt.org>
2012-04-07 03:58:22 +0530

    remove certain UI elements

--- editor/awwation.js
+++ editor/awwation.js
@@ -1,4 +1,4 @@
 $(function() {
-    $('#tool_wireframe, #main_button, #tool_zoom').remove();
+    $('#tool_wireframe, #main_button, #tool_zoom, #tool_topath, #tool_reorient, #idLabel, #tool_blur, #tool_position, #xy_panel, #tool_angle, #rect_panel, #circle_panel, #ellipse_panel, #line_panel, #multiselected_panel, #container_panel, #g_panel, #tools_rect, #tools_ellipse').remove();
     $('#tools_top').css('left', 5);
 });
